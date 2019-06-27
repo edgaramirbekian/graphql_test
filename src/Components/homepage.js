e4fd445193ce35629626d488de5dcf8cbb948334
Edgar <amirbekianedgar@gmail.com>
2019-06-27 10:30:48 +0400

    get and render the data from query

--- src/Components/homepage.js
+++ src/Components/homepage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { gql } from 'apollo-boost';
 import { graphql } from 'react-apollo';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const getTranslationQuery = gql `
     {
         contentText {
-            contentTextItem {
+            en {
                 key
                 value
             }
@@ -15,10 +16,42 @@ const getTranslationQuery = gql `
 
 const Homepage = (props) => {
     console.log(props)
+    const data = props.data;
+    console.log(data)
+    const displayContent = () => {
+        if (data.loading === true ) {
+            return (
+                <div>Loading...</div>
+            )
+        }
+        else if (data.loading === false && data.networkStatus === 7) {
+            const contentItems = data.contentText.en;
+            return contentItems.map((contentItem, idx) => {
+                return (
+                    <Row key={idx}>
+                        ---{idx}---
+                        <Col sm={3}>
+                            {contentItem.key}
+                        </Col>
+                        <Col sm={8}>
+                            {contentItem.value}
+                        </Col>
+                    </Row>
+                )
+            })
+        }
+        else {
+            return (
+                <div>Error :/</div>
+            )
+        }
+    };
+
     return (
-        <div>
-        </div>
-    );
+        <Container>
+            {displayContent()}
+        </Container>
+    )
 };
 
 export default graphql(getTranslationQuery)(Homepage);
